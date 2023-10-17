97c4143a22b0c4fe1ff56d2a7098b21646b8724b
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-10-16 23:29:05 -0700

    some thing

---
+++ js.js
@@ -0,0 +1,28 @@
+
+
+// result = Math.PI;
+// result = Math.E
+// result = Math.round(2.8)
+// result = Math.ceil(2.8)
+// result = Math.floor(2.8)
+// result = Math.sqrt(2.8)
+// result = Math.sqrt(2.8)
+// result = Math.pow(2,3)
+// result = Math.sqrt(2.8)
+// result = Math.min(2,8,3,4,5,11)
+// result = Math.random()
+
+
+let num = 4;
+if (num > 1){
+    console.log("over one");
+}
+
+
+
+
+
+
+
+
+// console.log(result);
